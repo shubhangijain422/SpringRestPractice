ddfa63777f1572dbc4926e560c8bafd24bf3f849
shubhangijain422 <shubhangijain422@gmail.com>
2021-02-18 18:06:05 +0530

    First Commit

---
+++ src/main/java/com/myweb/demo/ShubhangiFirstWebServiceProjectEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.myweb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShubhangiFirstWebServiceProjectEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShubhangiFirstWebServiceProjectEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myweb/demo/ShubhangiFirstWebServiceProjectEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myweb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShubhangiFirstWebServiceProjectEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
